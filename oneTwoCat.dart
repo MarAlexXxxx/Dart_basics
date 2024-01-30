78410838327b4c0cb7910bd9d56e5f55132c26e9
MarAlexXx <huligan2023@yandex.ru>
2024-01-30 15:26:26 +0300

    Английские слова для цифр от zero до nine

--- oneTwoCat.dart
+++ oneTwoCat.dart
@@ -13,27 +13,3 @@ class DigitExtractor {
       'eight': 8,
       'nine': 9,
     };
-
-    // Разбиваем строку на слова, удаляем лишние пробелы, приводим к нижнему регистру
-    List<String> words =
-        input.split(',').map((word) => word.trim().toLowerCase()).toList();
-
-    // Отображаем каждое слово на соответствующую цифру и убираем повторы
-    Set<int> uniqueDigits = words
-        .where((word) => englishWords.containsKey(word))
-        .map((word) => englishWords[word]!)
-        .toSet();
-
-    return uniqueDigits.toList();
-  }
-}
-
-void main() {
-  String inputString = "one, two, zero, zero, five";
-
-  // Извлекаем уникальные цифры из строки
-  List<int> extractedDigits = DigitExtractor.extractUniqueDigits(inputString);
-
-  // Выводим результат
-  print('Уникальные цифры: $extractedDigits');
-}
