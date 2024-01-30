df432b4f0f3db6ba0a07b4fd47072c6c8d3e1bfd
MarAlexXx <huligan2023@yandex.ru>
2024-01-30 15:24:28 +0300

    Метод для преобразования целого числа из двоичной системы в десятичную

--- dvoichnaya_sis.dart
+++ dvoichnaya_sis.dart
@@ -13,3 +13,17 @@ class BinaryConverter {
 
     return binary;
   }
+
+  // Метод для преобразования целого числа из двоичной системы в десятичную
+  static int binaryToDecimal(String binary) {
+    int decimal = 0;
+    int length = binary.length;
+
+    for (int i = 0; i < length; i++) {
+      int digit = int.parse(binary[length - 1 - i]);
+      decimal += digit * (1 << i);
+    }
+
+    return decimal;
+  }
+}
