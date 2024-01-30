8e96212cb4ab9ad7d34fe2fcebb6848eada8acad
MarAlexXx <huligan2023@yandex.ru>
2024-01-30 15:28:42 +0300

    Решение

--- oneTwoCat.dart
+++ oneTwoCat.dart
@@ -1,6 +1,5 @@
 class DigitExtractor {
   static List<int> extractUniqueDigits(String input) {
-    // Английские слова для цифр от zero до nine
     final englishWords = {
       'zero': 0,
       'one': 1,
@@ -14,11 +13,9 @@ class DigitExtractor {
       'nine': 9,
     };
 
-    // Разбиваем строку на слова, удаляем лишние пробелы, приводим к нижнему регистру
     List<String> words =
         input.split(',').map((word) => word.trim().toLowerCase()).toList();
 
-    // Отображаем каждое слово на соответствующую цифру и убираем повторы
     Set<int> uniqueDigits = words
         .where((word) => englishWords.containsKey(word))
         .map((word) => englishWords[word]!)
@@ -26,4 +23,14 @@ class DigitExtractor {
 
     return uniqueDigits.toList();
   }
+}
+
+void main() {
+  String inputString = "one, two, zero, zero, five";
+
+  // Извлекаем уникальные цифры из строки
+  List<int> extractedDigits = DigitExtractor.extractUniqueDigits(inputString);
+
+  // Выводим результат
+  print('Уникальные цифры: $extractedDigits');
 }
