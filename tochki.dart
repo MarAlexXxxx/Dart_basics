296d55e80489309c13d20358a7eb8874a5e3937c
MarAlexXx <huligan2023@yandex.ru>
2024-01-30 15:35:11 +0300

    Метод для вычисления расстояния между двумя точками

--- tochki.dart
+++ tochki.dart
@@ -13,8 +13,15 @@ class Point {
     return Point(1, 1, 1);
   }
 
-  // Factory-конструктор для создания единичного вектора в направлении заданной точки
   factory Point.unitVector(Point point) {
     final length = point.distanceTo(Point.origin());
     return Point(point.x / length, point.y / length, point.z / length);
+  }
+
+  // Метод для вычисления расстояния между двумя точками
+  double distanceTo(Point another) {
+    final dx = x - another.x;
+    final dy = y - another.y;
+    final dz = z - another.z;
+    return sqrt(dx * dx + dy * dy + dz * dz);
   }
