1483efa7a0baa2a70448019d2996855ebaf7ca56
MarAlexXx <huligan2023@yandex.ru>
2024-01-30 15:21:52 +0300

    Подсчитываем вхождения слов в коллекции

--- collect.dart
+++ collect.dart
@@ -1,6 +1,27 @@
 class WordCounter {
-  // Метод для подсчета вхождений слов в коллекции
   static Map<String, int> countWords(List<String> words) {
-    Map<String, int> wordCountMap = {};
+    Map<String, int> wordCountMap = {};  // Создаем пустой Map для хранения подсчета слов
 
+    for (String word in words) {
+      wordCountMap[word] = (wordCountMap[word] ?? 0) + 1;
+    }
 
+    return wordCountMap;  // Возвращаем Map с количеством вхождений каждого слова
+  }
+}
+
+void main() {
+  List<String> wordCollection = [
+    "apple",
+    "orange",
+    "apple",
+    "banana",
+    "orange",
+    "apple"
+  ];
+
+  // Подсчитываем вхождения слов в коллекции
+  Map<String, int> wordCountMap = WordCounter.countWords(wordCollection);
+
+  print('Количество вхождений слов в коллекции: $wordCountMap');
+}
