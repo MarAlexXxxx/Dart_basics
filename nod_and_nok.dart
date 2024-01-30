1848afcf881dcd776b9f7ba1ce513201020db8c6
MarAlexXx <huligan2023@yandex.ru>
2024-01-30 15:03:57 +0300

    b теперь становится остатком от деления a на b

--- nod_and_nok.dart
+++ nod_and_nok.dart
@@ -3,4 +3,5 @@ class MathUtils {
   static int gcd(int a, int b) {
     while (b != 0) {
       final int temp = b;  // Сохраняем значение b во временной переменной temp
+      b = a % b;           // b теперь становится остатком от деления a на b
      
