458e0af445baa47eab80eca11684e912ee4f4eff
MarAlexXx <huligan2023@yandex.ru>
2024-01-30 15:35:55 +0300

    Метод для вычисления площади треугольника, образованного тремя точками

--- tochki.dart
+++ tochki.dart
@@ -18,10 +18,19 @@ class Point {
     return Point(point.x / length, point.y / length, point.z / length);
   }
 
-  // Метод для вычисления расстояния между двумя точками
   double distanceTo(Point another) {
     final dx = x - another.x;
     final dy = y - another.y;
     final dz = z - another.z;
     return sqrt(dx * dx + dy * dy + dz * dz);
   }
+
+  // Метод для вычисления площади треугольника, образованного тремя точками
+  double triangleArea(Point second, Point third) {
+    final a = distanceTo(second);
+    final b = distanceTo(third);
+    final c = second.distanceTo(third);
+    final s = (a + b + c) / 2;
+    return sqrt(s * (s - a) * (s - b) * (s - c));
+  }
+}
