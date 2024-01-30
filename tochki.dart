a1b42f3d383e5c687cf1baebc2a4030c565fdd07
MarAlexXx <huligan2023@yandex.ru>
2024-01-30 15:33:07 +0300

    Factory-конструктор для создания точки в начале координат

--- tochki.dart
+++ tochki.dart
@@ -6,3 +6,7 @@ class Point {
   // Конструктор для создания точки с заданными координатами
   Point(this.x, this.y, this.z);
 
+  // Factory-конструктор для создания точки в начале координат
+  factory Point.origin() {
+    return Point(0, 0, 0);
+  }
