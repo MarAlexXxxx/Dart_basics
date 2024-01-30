8a314f443db0405b0876134de18f1bd807ab90c1
MarAlexXx <huligan2023@yandex.ru>
2024-01-30 15:20:43 +0300

    Если слово уже есть в Map, увеличиваем счетчик

--- collect.dart
+++ collect.dart
@@ -3,11 +3,4 @@ class WordCounter {
   static Map<String, int> countWords(List<String> words) {
     Map<String, int> wordCountMap = {};
 
-    for (String word in words) {
-      // Если слово уже есть в Map, увеличиваем счетчик
-      wordCountMap[word] = (wordCountMap[word] ?? 0) + 1;
-    }
 
-    return wordCountMap;
-  }
-}
